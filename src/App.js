caaf57603952c40281076d945919065cc1a8535c
Mark Bedner <mbedner@webstaurantstore.com>
2022-06-09 16:06:55 -0400

    second commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import { Player } from "./components/Player";
+import { Song } from "./components/Song";
+
+function App() {
+  return (
+    <div className="App">
+      <Song />
+      <Player />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Player.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+export const Player = () => {
+  return (
+    <div className="player">
+      <div className="time-control">
+        <p>Start time</p>
+        <input type="range" name="" id="" />
+        <p>End time</p>
+      </div>
+      <div className="play-control"></div>
+    </div>
+  );
+};
